078d6cf1cd1c48c81cafa0909a9532fd0ae0b613
Makoto Shiraishi <makotan96.mp@gmail.com>
2025-11-03 16:44:10 +0900

    chore: canonicalize experimental.allowedDevOrigins (localhost/127/192.168.0.23 × 3000/3001/3020/3030)

--- next.config.mjs
+++ next.config.mjs
@@ -2,21 +2,10 @@
 const nextConfig = {
   experimental: {
     allowedDevOrigins: [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "http://192.168.0.23:3000"
-  ,
-    "http://localhost:3001",
-    "http://127.0.0.1:3001",
-    "http://192.168.0.23:3001"
-  ,
-    "http://localhost:3001",
-    "http://127.0.0.1:3001",
-    "http://192.168.0.23:3001"
-  ,
-    "http://localhost:3020", "http://127.0.0.1:3020", "http://192.168.0.23:3020",
-    "http://localhost:3030", "http://127.0.0.1:3030", "http://192.168.0.23:3030"
-  ]
-  }
+      "http://localhost",           "http://localhost:3000", "http://localhost:3001", "http://localhost:3020", "http://localhost:3030",
+      "http://127.0.0.1",           "http://127.0.0.1:3000", "http://127.0.0.1:3001", "http://127.0.0.1:3020", "http://127.0.0.1:3030",
+      "http://192.168.0.23",        "http://192.168.0.23:3000", "http://192.168.0.23:3001", "http://192.168.0.23:3020", "http://192.168.0.23:3030",
+    ],
+  },
 };
 export default nextConfig;
