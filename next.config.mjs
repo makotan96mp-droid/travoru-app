9a1e5a904815431cd10e6d39f6a3264dbe232c5d
Makoto Shiraishi <makotan96.mp@gmail.com>
2025-11-03 15:50:04 +0900

    chore: normalize experimental.allowedDevOrigins (localhost/127.0.0.1/192.168.0.23)

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    allowedDevOrigins: ['http://192.168.0.23:3000', 'http://192.168.0.23']
+    allowedDevOrigins: [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://192.168.0.23:3000"
+  ]
   }
 };
 export default nextConfig;
